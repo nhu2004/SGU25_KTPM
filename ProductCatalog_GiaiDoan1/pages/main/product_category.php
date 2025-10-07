4227033744d195c019d646c454e7896671c4d245
HoanBau <nguyenhoanbau2004@gmail.com>
2025-10-07 21:08:57 +0700

    Code Danh Mục Sản Phẩm
    Giai Đoạn 1 - (8/10)

---
+++ ProductCatalog_GiaiDoan1/pages/main/product_category.php
@@ -0,0 +1,12 @@
+<!-- start product banner -->
+<?php include(__DIR__ . '/../base/product-banner.php'); ?>
+<!-- end product banner -->
+
+<!-- start product sort -->
+<?php include(__DIR__ . '/../base/product-sort.php'); ?>
+<!-- end product sort -->
+
+<!-- start product list -->
+<?php include(__DIR__ . '/../base/product-list.php'); ?>
+<!-- end product list -->
+
